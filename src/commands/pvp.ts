660b953b3de85b46f7461102c345ce193a1f9969
fenixo76 <57808345+fenixo76@users.noreply.github.com>
2022-06-07 21:19:12 +0200

    Update pvp.ts

--- src/commands/pvp.ts
+++ src/commands/pvp.ts
@@ -14,7 +14,7 @@ export default {
         const embed = new MessageEmbed()
             .setColor("#CC0000")
             .setAuthor({ name: "Sondage", iconURL: "https://titles.trackercdn.com/destiny/common/destiny2_content/icons/DestinyActivityModeDefinition_fb3e9149c43f7a2e8f8b66cbea7845fe.png" })
-            .setDescription("<@&804813354493280276> \nVeuillez répondre à ce sondage pas le biais de ces réaction :\n👍 ou 👎")
+            .setDescription("<@&804813354493280276> \nVeuillez répondre à ce sondage par le biais de ces réactions :\n👍 ou 👎")
             .setThumbnail("https://titles.trackercdn.com/destiny/common/destiny2_content/icons/DestinyActivityModeDefinition_fb3e9149c43f7a2e8f8b66cbea7845fe.png")
             .setTitle("Qui veut faire du PVP ?")
 
