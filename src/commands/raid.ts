c85aed0914d850579433bc05874bea21e371fba4
fenixo76 <57808345+fenixo76@users.noreply.github.com>
2022-06-08 14:18:27 +0200

    Update raid.ts

--- src/commands/raid.ts
+++ src/commands/raid.ts
@@ -14,32 +14,36 @@ export default {
         .addComponents(
             new MessageSelectMenu()
                 .setCustomId("select")
-                .setPlaceholder("Sélectionnez l'heure du RAID")
+                .setPlaceholder("Sélectionnez le temps ou l'heure du RAID")
                 .addOptions([
                     {
-                        label: "À",
-                        description: "21h00",
-                        value: "21h00 ?",
+                        label: "dans",
+                        description: "10m",
+                        value: "dans 10 minute ?",
                         
                     },
-                  
                     {
-                        label: "À",
-                        description: "21h30",
-                        value: "21h30 ?",
-                    },
-                    {
-                        label: "À",
-                        description: "22h00",
-                        value: "22h00",
+                        label: "dans",
+                        description: "30m",
+                        value: "dans 30 minutes ?",
                         
                     },
-                  
                     {
-                        label: "À",
-                        description: "22h30",
-                        value: "22h30",
+                        label: "dans",
+                        description: "1h00",
+                        value: "dans 1h00 ?",
                     },
+                    {
+                        label: "ce",
+                        description: "soir à 21h00",
+                        value: "ce soir à 21h00 ?",
+                    },
+                    {
+                        label: "ce",
+                        description: "soir à 21h30",
+                        value: "ce soir à 21h30 ou plus ?",
+                    },
+
                 ])
         )
         const embed = new MessageEmbed()
@@ -47,7 +51,7 @@ export default {
             .setAuthor({ name: "Sondage", iconURL: "https://titles.trackercdn.com/destiny/common/destiny2_content/icons/DestinyActivityModeDefinition_bfe80e3dafe6686a9dc42df0606bdc9b.png" })
             .setDescription("<@&804813354493280276> \nVeuillez répondre à ce sondage par le biais de ces réactions :\n👍 ou 👎")
             .setThumbnail("https://titles.trackercdn.com/destiny/common/destiny2_content/icons/DestinyActivityModeDefinition_bfe80e3dafe6686a9dc42df0606bdc9b.png")
-            .setTitle("Qui veut faire un raid à")
+            .setTitle("Qui veut faire un raid")
 
         const message = await interaction.reply({embeds: [embed], components: [row], fetchReply: true }) as Message
         message.react("👍")
