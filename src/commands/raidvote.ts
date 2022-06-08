18f5133a06f8bf59508736e201d6e5411c6043cd
fenixo76 <57808345+fenixo76@users.noreply.github.com>
2022-06-08 14:19:07 +0200

    Update raidvote.ts

--- src/commands/raidvote.ts
+++ src/commands/raidvote.ts
@@ -14,32 +14,36 @@ export default {
         .addComponents(
             new MessageSelectMenu()
                 .setCustomId("select")
-                .setPlaceholder("Sélectionnez l'heure du RAID")
+                .setPlaceholder("Sélectionnez le temps ou l'heure du RAID")
                 .addOptions([
                     {
-                        label: "À",
-                        description: "21h00",
-                        value: "21h00 ?",
+                        label: "dans",
+                        description: "10m",
+                        value: "dans 10 minute ?",
                         
                     },
-                  
                     {
-                        label: "À",
-                        description: "21h30",
-                        value: "21h30 ?",
-                    },
-                    {
-                        label: "À",
-                        description: "22h00",
-                        value: "22h00",
+                        label: "dans",
+                        description: "30m",
+                        value: "dans 30 minutes ?",
                         
                     },
-                  
                     {
-                        label: "À",
-                        description: "22h30",
-                        value: "22h30",
+                        label: "dans",
+                        description: "1h00",
+                        value: "dans 1h00 ?",
                     },
+                    {
+                        label: "ce",
+                        description: "soir à 21h00",
+                        value: "ce soir à 21h00 ?",
+                    },
+                    {
+                        label: "ce",
+                        description: "soir à 21h30",
+                        value: "ce soir à 21h30 ou plus ?",
+                    },
+
                 ])
         )
         const embed = new MessageEmbed()
@@ -47,7 +51,7 @@ export default {
             .setAuthor({ name: "Sondage", iconURL: "https://titles.trackercdn.com/destiny/common/destiny2_content/icons/DestinyActivityModeDefinition_bfe80e3dafe6686a9dc42df0606bdc9b.png" })
             .setDescription("<@&804813354493280276> \nVeuillez répondre à ce sondage par le biais de ces réactions :\n--------------------------------------------------------------------")
             .setThumbnail("https://titles.trackercdn.com/destiny/common/destiny2_content/icons/DestinyActivityModeDefinition_bfe80e3dafe6686a9dc42df0606bdc9b.png")
-            .setTitle("On fait quelle RAID à")
+            .setTitle("On fait quelle RAID")
             .addFields(
                 { name: "Jardin du salut", value: "Votez avec : 🤬", inline: true },
                 { name: "Dernier voeux", value: "Votez avec : 🤯", inline: true },
