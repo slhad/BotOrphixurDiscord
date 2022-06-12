3824393e98410408b17cc160940da483ae05649a
fenixo76 <57808345+fenixo76@users.noreply.github.com>
2022-06-12 12:48:10 +0200

    Update gambit.ts

--- src/commands/gambit.ts
+++ src/commands/gambit.ts
@@ -1,5 +1,6 @@
 import { SlashCommandBuilder } from "@discordjs/builders"
 import { CommandInteraction, Message, MessageActionRow, MessageEmbed, MessageSelectMenu } from "discord.js"
+import { destinyIcons, membresTeam } from "../constantes"
 
 export default {
     data: new SlashCommandBuilder()
@@ -48,9 +49,9 @@ export default {
         )
         const embed = new MessageEmbed()
             .setColor("#00FF08")
-            .setAuthor({ name: "Sondage", iconURL: "https://titles.trackercdn.com/destiny/common/destiny2_content/icons/DestinyActivityModeDefinition_96f7e9009d4f26e30cfd60564021925e.png" })
-            .setDescription("<@&804813354493280276> \nVeuillez répondre à ce sondage par le biais de ces réactions :\n👍 ou 👎")
-            .setThumbnail("https://titles.trackercdn.com/destiny/common/destiny2_content/icons/DestinyActivityModeDefinition_96f7e9009d4f26e30cfd60564021925e.png")
+            .setAuthor({ name: "Sondage", iconURL: `${destinyIcons}/DestinyActivityModeDefinition_96f7e9009d4f26e30cfd60564021925e.png`})
+            .setDescription(`${membresTeam} \nVeuillez répondre à ce sondage par le biais de ces réactions :\n👍 ou 👎`)
+            .setThumbnail(`${destinyIcons}/DestinyActivityModeDefinition_96f7e9009d4f26e30cfd60564021925e.png`)
             .setTitle("Qui veut faire du Gambit ?")
 
         const message = await interaction.reply({ embeds: [embed],components: [row], fetchReply: true }) as Message
