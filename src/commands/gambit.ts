6af05c653059896cb6df943b1739feefe9a77eeb
fenixo76 <57808345+fenixo76@users.noreply.github.com>
2022-06-14 09:04:07 +0200

    Update gambit.ts

--- src/commands/gambit.ts
+++ src/commands/gambit.ts
@@ -1,6 +1,6 @@
 import { SlashCommandBuilder } from "@discordjs/builders"
 import { CommandInteraction, Message, MessageActionRow, MessageEmbed, MessageSelectMenu } from "discord.js"
-import { destinyIcons, membresTeam } from "../constantes"
+import { destinyIcons, membresTeam, voter } from "../constantes"
 
 export default {
     data: new SlashCommandBuilder()
@@ -50,7 +50,7 @@ export default {
         const embed = new MessageEmbed()
             .setColor("#00FF08")
             .setAuthor({ name: "Sondage", iconURL: `${destinyIcons}/DestinyActivityModeDefinition_96f7e9009d4f26e30cfd60564021925e.png`})
-            .setDescription(`${membresTeam} \nVeuillez répondre à ce sondage par le biais de ces réactions :\n👍 ou 👎`)
+            .setDescription(`${membresTeam} ${voter}`)
             .setThumbnail(`${destinyIcons}/DestinyActivityModeDefinition_96f7e9009d4f26e30cfd60564021925e.png`)
             .setTitle("Qui veut faire du Gambit ?")
 
