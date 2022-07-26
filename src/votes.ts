592353fad60ef7c1481fdf5f88b0891293d76cad
fenixo76 <57808345+fenixo76@users.noreply.github.com>
2022-07-26 18:29:39 +0200

    Create votes.ts

---
+++ src/votes.ts
@@ -0,0 +1,12 @@
+import { Message } from "discord.js"
+
+export const messageCreate = async (message:Message) => {
+    if (message.channel.id === "980469927075020820") {
+        message.react("0️⃣")
+        message.react("1️⃣")
+        message.react("2️⃣")
+        message.react("3️⃣")
+        message.react("4️⃣")
+        message.react("5️⃣")
+    }
+}
