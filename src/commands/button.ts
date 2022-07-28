ee16f0b3479cab5bfee94e2b39c540147074efc9
fenixo76 <57808345+fenixo76@users.noreply.github.com>
2022-07-28 16:25:54 +0200

    Update button.ts

--- src/commands/button.ts
+++ src/commands/button.ts
@@ -1,36 +1,53 @@
 import { SlashCommandBuilder } from "@discordjs/builders"
-import { Message, MessageEmbed} from "discord.js"
-import { buttonsPagination } from "../helpers/buttonPagination"
+import { MessageEmbed, CommandInteraction} from "discord.js"
+import { sendPaginatedEmbeds } from "discord.js-embed-pagination"
+
 
 export default {
     data: new SlashCommandBuilder()
-        .setName("button")
-        .setDescription("test button"),
+        .setName("banshee")
+        .setDescription("information sur les article vendue par banshee"),
 
-    async execute(msg:Message) {
+        /**
+        * 
+        * @param {CommandInteraction} interaction 
+        */
+         async execute(interaction: CommandInteraction)  {
 
-        const page1 = new MessageEmbed()
-            .setTitle("page 1")
-            .setDescription("test page 1")
+               const titles = [{
+                    title: "Titre 1",
+                    thumbnail: "https://www.light.gg/Content/Images/lost-sector-icon.png",
+                    des: "Description 1",
+                    fieldsN: "Fieds 1",
+                    fieldsV: "value 1"
+                },{
+                    title: "Titre 2",
+                    thumbnail: "https://www.light.gg/Content/Images/nightfall-small-billboard-icon.png",
+                    des: "Description 2",
+                    fieldsN: "Fieds 2",
+                    fieldsV: "value 2"
+                },{
+                    title: "Titre 3",
+                    thumbnail: "https://www.light.gg/Content/Images/ada-icon.png",
+                    des: "Description 3",
+                    fieldsN: "Fieds 3",
+                    fieldsV: "value 3"
+                },{
+                    title: "Titre 4",
+                    thumbnail: "https://www.light.gg/Content/Images/banshee-icon.png",
+                    des: "Description 4",
+                    fieldsN: "Fieds 4",
+                    fieldsV: "value 4"
+                }]
 
-        const page2 = new MessageEmbed()
-            .setTitle("page 2")
-            .setDescription("test page 2")
 
-        const page3 = new MessageEmbed()
-            .setTitle("page 3")
-            .setDescription("test page 3")
+            const embeds =  titles.map(title => new MessageEmbed().setTitle(title.title)
+            .setThumbnail(title.thumbnail)
+            .setDescription(title.des)
+            .addFields({name: title.fieldsN, value:title.fieldsV, inline: true})
+            )
 
-        const page4 = new MessageEmbed()
-            .setTitle("page 4")
-            .setDescription("test page 4")
-
-        const pages = [ page1, page2, page3, page4]
-
-        const time = 30000
-
-
-        buttonsPagination(msg,pages,time) 
-       
+            await sendPaginatedEmbeds(interaction, embeds)
+            }
     }
 }
