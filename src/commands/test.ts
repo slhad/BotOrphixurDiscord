e07ad7825953092262415b8ba8fa44e2b1a477ea
fenixo76 <57808345+fenixo76@users.noreply.github.com>
2022-10-01 17:26:50 +0200

    Update test.ts

--- src/commands/test.ts
+++ src/commands/test.ts
@@ -1,8 +1,8 @@
 import { SlashCommandBuilder } from "@discordjs/builders"
-import { CommandInteraction,  MessageEmbed } from "discord.js"
+import { CommandInteraction} from "discord.js"
 import db from "../index"
 import { bungieAuthedFetch } from "../helpers/api"
-import { getProfile, getVendors } from "bungie-api-ts/destiny2/api"
+import { getVendors } from "bungie-api-ts/destiny2/api"
 import { DestinyComponentType } from "bungie-api-ts/destiny2"
 
 export default {
@@ -21,43 +21,28 @@ export default {
             if(err)
                 throw err
               
-                const access_Token = data.Token
+                const accessToken:string = data.Token
                 const members:string = data.UserB
                 const ShipT:number = data.shipsT
+                const character:string= data.Character
 
-                 const getDestinyProfile = async () => {
-                  return getProfile(bungieAuthedFetch(access_Token), {
-                    membershipType: ShipT,
-                    destinyMembershipId: members,
-                    components: [DestinyComponentType.Characters,DestinyComponentType.Profiles]
-                  })
-                }
-                const responce = await getDestinyProfile()
-                const getIdCharter = responce.Response.profile.data?.characterIds[0]
-                const MembersId = (JSON.stringify(getIdCharter)).replace("\"","").replace("\"","")
-                console.log(MembersId)
-
-                const getVendorS = async() =>{
-                  return getVendors(bungieAuthedFetch(access_Token), {
-                    characterId:MembersId,
+                const getVendorS = async() => {
+                  return getVendors(bungieAuthedFetch(accessToken), {
+                    characterId:character,
                     membershipType:ShipT,
                     destinyMembershipId:members,
-                    components:[DestinyComponentType.Vendors,DestinyComponentType.VendorSales]
-                  })
+                    components:[DestinyComponentType.Vendors]
+                  }
+                  )
+                 
                 }
                 const response2 = await getVendorS()
                 const test = response2.Response
               console.log(test)
 
-        const embed = new MessageEmbed()
-        .setColor("#FF00D8")
-        .setAuthor({ name: "Orphi Xur",iconURL:"https://cdn.discordapp.com/app-icons/980069176561516604/46897437315f9176ef65d5aedc2a7697.png?size=256"})
-        .setDescription("Connecte toi :  \n  [ICI](https://localhost:8000/register-start?discordId=)")
-        .setThumbnail("https://cdn.discordapp.com/app-icons/980069176561516604/46897437315f9176ef65d5aedc2a7697.png?size=256")
-        .setTitle("Connect toi et approuve moi !")
 
-        await interaction.reply({ embeds: [embed], fetchReply: true })
+
+        await interaction.reply({ fetchReply: true })
         }
     )}
-
   }
