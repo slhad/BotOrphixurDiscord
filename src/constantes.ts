d7ea762bb6f18233726b6d56282060b8f66ed846
Sylvain Rivierre <slhad@users.noreply.github.com>
2022-10-25 01:42:11 +0200

    Meilleure organisation de constantes

--- src/constantes.ts
+++ src/constantes.ts
@@ -3,9 +3,17 @@ export const destinyBasePath = "https://www.bungie.net/"
 export const destinyIcons2 = `${destinyBasePath}common/destiny2_content/icons`
 export const membresTeam = "<@&804813354493280276>"
 export const voter = "Veuillez répondre à ce sondage par le biais de ces réactions :"
+
+export const Vendor = {
+"banshee" : "672118013",
+  "xur" : "2190858386",
+  "ada_1" : "350061650"
+}
+
 export const bansheeV = "672118013"
 export const xurV = "2190858386"
 export const ada_1V = "350061650"
+
 export const lumen = "<:36dd52a3005cb28f7f68448f1ab76c1e:1030916978640175175>"
 export const eclat = "<:1ff4a008cb851794ccb588e4eb3918a1:1030917987689046069>"
 export const Abyssaux = "Abyssaux <:DestinyDamageTypeDefinition_ceb2:1031588260494979133>"
@@ -13,6 +21,14 @@ export const Cryo_électriques = "Cryo_électrique <:DestinyDamageTypeDefinition
 export const Solaires = "Solaire <:DestinyDamageTypeDefinition_2a17:1031588220980445275>"
 export const Stase = "Stasique <:DestinyDamageTypeDefinition_530c:1031588189145673840>"
 export const Cinétiques = "Cinétique <:DestinyDamageTypeDefinition_3385:1031588242371391560>"
+
+export const XurLocalisation = {
+  "Tour" : "La dernière Cité à la **Tour**"
+  "ZME" : "La Zone mort européenne **(ZME)**"
+  "Nessos" : "La vallée d'arcadia sur **Nessos**"
+}
+
+
 export const Tour = "La dernière Cité à la **Tour**"
 export const ZME = "La Zone mort européenne **(ZME)**"
 export const Nessos = "La vallée d'arcadia sur **Nessos**"
